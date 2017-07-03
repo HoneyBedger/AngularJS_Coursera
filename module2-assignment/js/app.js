caa7e8ce004cb9e7bb033a920824c267d196a9c1
Darya Komsa <komsa.darya@gmail.com>
2017-07-03 10:31:16 -0400

    Added module2 solution

---
+++ module2-assignment/js/app.js
@@ -0,0 +1,62 @@
+(function () {
+  'use strict';
+
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var buyCtrl = this;
+    buyCtrl.items = ShoppingListCheckOffService.getBuyItems();
+
+    buyCtrl.moveItem = function (itemIndex) {
+      ShoppingListCheckOffService.moveItem(itemIndex);
+      }
+    }
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var boughtCtrl = this;
+    boughtCtrl.items = ShoppingListCheckOffService.getBoughtItems();
+    console.log(boughtCtrl.items);
+  }
+
+  function ShoppingListCheckOffService () {
+    var shoppingService = this;
+    var buy = [
+      {name: 'cherries',
+       quantity: '3 lb of'},
+      {name: 'Black Forest cake',
+       quantity: '1'},
+      {name: 'cucumbers',
+       quantity: '2'},
+      {name: 'milk',
+       quantity: '4 L of'},
+      {name: 'potatoes',
+       quantity: '10'},
+      {name: 'sugar',
+       quantity: '2 bags of'},
+      {name: 'cherry sundae',
+       quantity: 'a bowl of'},
+      {name: 'pineapple',
+       quantity: '1'}
+    ];
+    var bought = [];
+
+    shoppingService.moveItem = function (itemIndex) {
+      var item = buy.splice(itemIndex, 1)[0];
+      bought.push(item);
+    };
+
+    shoppingService.getBuyItems = function () {
+      return buy;
+    };
+
+    shoppingService.getBoughtItems = function () {
+      return bought;
+    };
+  }
+
+})();
